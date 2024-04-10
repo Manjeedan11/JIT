d5e0eb44bdb7bd96567d8945969d5b602b462364
unknown <shakthivelmanjeedan11@gmail.com>
2024-04-10 21:50:03 +0530

    first commit

---
+++ app/src/main/java/com/affc/ait/Database.java
@@ -0,0 +1,57 @@
+package com.affc.ait;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class Database extends SQLiteOpenHelper {
+    public static final String DBname = "register.db";
+    SQLiteDatabase sqLiteDatabase;
+
+    public Database(@Nullable Context context) {
+        super(context, DBname, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        sqLiteDatabase.execSQL("create table users(username TEXT primary key, password TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        sqLiteDatabase.execSQL("drop table if exists users");
+    }
+
+    public boolean insertData(String username, String password){
+        SQLiteDatabase myDB = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("username", username);
+        contentValues.put("password", password);
+        long result = myDB.insert("users", null, contentValues);
+        if(result == -1) return false;
+        else return true;
+    }
+
+    public boolean checkUsername(String username){
+        SQLiteDatabase myDB = this.getWritableDatabase();
+        Cursor cursor = myDB.rawQuery("select * from users username = ?", new String[]{username});
+        if(cursor.getCount()>0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean checkUser(String username, String pwd){
+        SQLiteDatabase myDB = this.getWritableDatabase();
+        Cursor cursor = myDB.rawQuery("select * from users where username = ? and password = ?", new String[]{username, pwd});
+        if(cursor.getCount()>0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
---
+++ app/src/main/java/com/affc/ait/Login.java
@@ -0,0 +1,43 @@
+package com.affc.ait;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login extends AppCompatActivity {
+
+    Database db;
+    Button btnLogin;
+    EditText etUsername, etPwd;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        db = new Database(this);
+        etUsername = findViewById(R.id.etUsername);
+        etPwd = findViewById(R.id.etPassword);
+        btnLogin = findViewById((R.id.btnLogin));
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                  boolean isLoggedId = db.checkUser(etUsername.getText().toString(), etPwd.getText().toString());
+                  if(isLoggedId){
+                      Intent intent = new Intent(Login.this, Home.class);
+                      startActivity(intent);
+                  }
+                  
+                  else {
+                      Toast.makeText(Login.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                  }
+            }
+        });
+    }
+}
