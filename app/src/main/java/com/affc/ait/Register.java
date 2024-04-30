d32c9200d677d8158c3b1b12615cc79688d3e133
Isuru Yahampath <isuruyahampath1@gmail.com>
2024-04-30 23:00:40 +0530

    added databasehandler

--- app/src/main/java/com/affc/ait/Register.java
+++ app/src/main/java/com/affc/ait/Register.java
@@ -1,19 +1,14 @@
 package com.affc.ait;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Register extends AppCompatActivity {
-    Database dbHelper;
+    Database_OLDCODE dbHelper;
     String[] genders = {"Male", "Female", "Other"};
 
     @Override
@@ -21,7 +16,7 @@ public class Register extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, genders);
         setContentView(R.layout.activity_register);
-        dbHelper = new Database(this);
+        dbHelper = new Database_OLDCODE(this);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         Spinner spinner = findViewById(R.id.gender);
         spinner.setAdapter(adapter);
---
+++ app/src/main/java/com/affc/ait/db/DatabaseHandler.java
@@ -0,0 +1,88 @@
+package com.affc.ait.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHandler extends SQLiteOpenHelper {
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "JIT";
+
+
+    public DatabaseHandler(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        //3rd argument to be passed is CursorFactory instance
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        //Create the tables
+
+        final String sqlForStudent = "CREATE TABLE Student (" +
+                "    Student_ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "    name TEXT,          " +
+                "    email TEXT,         " +
+                "    login_code TEXT,         " +
+                "    address TEXT,        " +
+                "    city TEXT,        " +
+                "    phone TEXT,          " +
+                "    gender TEXT,             " +
+                "    DOB TEXT                    " +
+                ");";
+        final String sqlForAdmin = "CREATE TABLE Admin (" +
+                "admin_ID INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "admin_email TEXT," +
+                "admin_password TEXT" +
+                ");";
+        final String sqlForBranch = "CREATE TABLE Branch (" +
+                "branch_ID INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "branch_name TEXT," +
+                "location TEXT" +
+                ");";
+
+        final String sqlForCourse = "CREATE TABLE Course (" +
+                "     course_ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "    course_name TEXT,          " +
+                "    start_date TEXT,         " +
+                "    end_date TEXT,        " +
+                "    fee REAL,        " +
+                "    description TEXT,          " +
+                "    max_p INTEGER             " +
+                ");";
+        sqLiteDatabase.execSQL(sqlForStudent);
+        sqLiteDatabase.execSQL(sqlForBranch);
+        sqLiteDatabase.execSQL(sqlForCourse);
+        sqLiteDatabase.execSQL(sqlForAdmin);
+    }
+
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        // Drop older table if existed
+        sqLiteDatabase.execSQL("DROP TABLE Student");
+        sqLiteDatabase.execSQL("DROP TABLE Admin");
+        sqLiteDatabase.execSQL("DROP TABLE Branch");
+        sqLiteDatabase.execSQL("DROP TABLE Course");
+        // Create tables again
+        onCreate(sqLiteDatabase);
+    }
+
+    /*
+    list of methods needed to work with the db:
+    add user
+    add profile picture
+    verify user email
+    generate and update login code
+
+    fetch all courses (limit to 10)
+    query for courses by name
+    fetch single course info
+    fetch branches for given course
+
+    verify admin
+    add new course
+    add new branch
+    fetch all users (limit to 10)
+    query for users by name
+     */
+}
