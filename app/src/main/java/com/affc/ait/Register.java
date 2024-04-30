a785a1c2e343e8aefe0e90db3eef665693b3834a
Isuru Yahampath <isuruyahampath1@gmail.com>
2024-04-30 07:30:02 +0530

    register ui done

--- app/src/main/java/com/affc/ait/Register.java
+++ app/src/main/java/com/affc/ait/Register.java
@@ -3,8 +3,10 @@ package com.affc.ait;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -12,11 +14,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Register extends AppCompatActivity {
     Database dbHelper;
+    String[] genders = {"Male", "Female", "Other"};
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, genders);
         setContentView(R.layout.activity_register);
         dbHelper = new Database(this);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner spinner = findViewById(R.id.gender);
+        spinner.setAdapter(adapter);
     }
 }
