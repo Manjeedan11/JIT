3e6b8af7ac3eb665b633fbd0e3d8f34665482ac1
Isuru Yahampath <isuruyahampath1@gmail.com>
2024-05-02 09:08:58 +0530

    major updates everywhere

--- app/src/main/java/com/affc/ait/Login.java
+++ app/src/main/java/com/affc/ait/Login.java
@@ -1,32 +1,129 @@
 package com.affc.ait;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.affc.ait.db.DatabaseHandler;
+import com.affc.ait.models.Student;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.ActionCodeSettings;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
+import java.util.Properties;
 
 public class Login extends AppCompatActivity {
     Database_OLDCODE db;
     Button btnLogin;
 
+    private String email;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+//        FirebaseApp.initializeApp(this);
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        Intent intent = getIntent();
+        if(intent.getData() !=null ) {
+            handleSignInIntent(intent);
+        }
+
+    }
+
+    public void handleSignInIntent(Intent intent) {
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        String emailLink = intent.getData().toString();
+
+// Confirm the link is a sign-in with email link.
+        if (auth.isSignInWithEmailLink(emailLink)) {
+            // Retrieve this from wherever you stored it
+
+            // The client SDK will parse the code from the link for you.
+            auth.signInWithEmailLink(email, emailLink)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Log.d(TAG, "Successfully signed in with email link!");
+                                AuthResult result = task.getResult();
+                                // You can access the new user via result.getUser()
+                                // Additional user info profile *not* available via:
+                                // result.getAdditionalUserInfo().getProfile() == null
+                                // You can check if the user is new or existing:
+                                // result.getAdditionalUserInfo().isNewUser()
+                            } else {
+                                Log.e(TAG, "Error signing in with email link", task.getException());
+                            }
+                        }
+                    });
+        }
     }
 
 
     public void sendLoginCode(View view){
-        String email = findViewById(R.id.email).toString();
+        EditText emailField = findViewById(R.id.email);
+        email = emailField.getText().toString();
+        if(email.isEmpty()){
+            showErrorMessage("Email is required");
+            return;
+        }
+
+        DatabaseHandler dbHandler = new DatabaseHandler(this);
+        int id = dbHandler.verifyEmailExists(email);
+        Log.e("id", "Email: " + email + " id: " + id);
+        if (id != -1){
+            Student student = new Student(id);
+//            String code = dbHandler.updateLoginCode(student);
+
+            ActionCodeSettings actionCodeSettings =
+                    ActionCodeSettings.newBuilder()
+                            // URL you want to redirect back to. The domain (www.example.com) for this
+                            // URL must be whitelisted in the Firebase Console.
+                            .setUrl("https://affc.page.link")
+                            // This must be true
+                            .setHandleCodeInApp(true)
+                            .setIOSBundleId("com.affc.ait")
+                            .setAndroidPackageName(
+                                    "com.affc.ait",
+                                    true, /* installIfNotAvailable */
+                                    "12"    /* minimumVersion */)
+                            .build();
+
+            FirebaseAuth auth = FirebaseAuth.getInstance();
+            auth.sendSignInLinkToEmail(email, actionCodeSettings)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Log.d(TAG, "Email sent.");
+                            }
+                        }
+                    });
 
 
+        } else {
+            showErrorMessage("Email not found");
+        }
     }
+
+
 
 
     private void showErrorMessage(String message) {
--- app/src/main/java/com/affc/ait/SetupProfileActivity.java
+++ app/src/main/java/com/affc/ait/SetupProfileActivity.java
@@ -1,14 +1,120 @@
 package com.affc.ait;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+
+import com.affc.ait.db.DatabaseHandler;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class SetupProfileActivity extends AppCompatActivity {
+
+    private long id;
+    private static final int REQUEST_IMAGE_CAPTURE = 101;
+    private static final int REQUEST_IMAGE_PICK = 102;
+    private static final int PERMISSION_REQUEST_CODE = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup_profile);
+
+        Intent intent = getIntent();
+        id = intent.getLongExtra("student_id", -1);
     }
+
+    public void redirectToLogin(View view) {
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == REQUEST_IMAGE_CAPTURE) {
+                Bitmap imageBitmap = (Bitmap) data.getExtras().get("data");
+                saveImageToStorage(imageBitmap);
+            } else if (requestCode == REQUEST_IMAGE_PICK) {
+                Uri selectedImageUri = data.getData();
+                saveImageToStorage(selectedImageUri);
+            }
+        }
+    }
+
+    private void saveImageToStorage(Object imageData) {
+        if (imageData != null) {
+            OutputStream outputStream;
+            String imageFileName = "IMG_" + System.currentTimeMillis() + ".jpg";
+            File storageDir = getExternalFilesDir(android.os.Environment.DIRECTORY_PICTURES);
+
+            if (imageData instanceof Bitmap) {
+                Bitmap imageBitmap = (Bitmap) imageData;
+                File imageFile = new File(storageDir, imageFileName);
+                try {
+                    outputStream = new FileOutputStream(imageFile);
+                    imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+                    outputStream.close();
+                    // Save image path to database
+                    saveImagePathToDatabase(imageFile.getAbsolutePath());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else if (imageData instanceof Uri) {
+                Uri selectedImageUri = (Uri) imageData;
+                File imageFile = new File(storageDir, imageFileName);
+                try {
+                    outputStream = new FileOutputStream(imageFile);
+                    outputStream.write(getBytesFromUri(selectedImageUri));
+                    outputStream.close();
+                    // Save image path to database
+                    saveImagePathToDatabase(imageFile.getAbsolutePath());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    private void saveImagePathToDatabase(String imagePath) {
+        if(id == -1) {
+            return;
+        }
+        DatabaseHandler dbHandler = new DatabaseHandler(this);
+        dbHandler.addProfilePicture((int) id, imagePath);
+    }
+
+    private byte[] getBytesFromUri(Uri uri) throws IOException {
+        try (InputStream inputStream = getContentResolver().openInputStream(uri)) {
+            ByteArrayOutputStream byteBuffer = new ByteArrayOutputStream();
+            int bufferSize = 1024;
+            byte[] buffer = new byte[bufferSize];
+            int len;
+            while ((len = inputStream.read(buffer)) != -1) {
+                byteBuffer.write(buffer, 0, len);
+            }
+            return byteBuffer.toByteArray();
+        }
+    }
+
+
+
+
 }
